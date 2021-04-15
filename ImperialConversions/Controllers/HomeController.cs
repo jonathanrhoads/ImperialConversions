8a1c4b59a73a20bf241bb7deeb2a133a9d2350a3
Jonathan Rhoads <jonathan.r.rhoads@outlook.com>
2021-04-15 13:49:17 -0500

    Add project files.

---
+++ ImperialConversions/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using ImperialConversions.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImperialConversions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Lengths lengths = new Lengths();
+
+            return View(lengths);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Lengths length)
+        {
+            if(length.conversion == true)
+            {
+                length.answer = length.from * 12;
+            }
+            else
+            {
+                length.answer = length.from / 12;
+            }
+            
+            return View(length);
+            
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
---
+++ ImperialConversions/Models/Lengths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImperialConversions.Models
+{
+    public class Lengths
+    {
+        public double from { get; set; }
+
+        public bool conversion { get; set; }
+        
+        public double answer { get; set; }
+
+   
+    }
+}
